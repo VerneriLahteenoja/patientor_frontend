0b0b05144b4c49b9c181742013e7ce31299c52ef
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-10-01 21:01:41 +0300

    entryForm skeleton

---
+++ src/components/PatientPage/entryForm.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+
+const EntryForm = () => {
+  const [description, setDescription] = useState('');
+  const [date, setDate] = useState('');
+  const [specialist, setSpecialist] = useState('');
+  const [healthCheckRating, setHealthCheckRating] = useState('');
+
+  const onSubmit = () => {
+    //TODO: implement form submit
+    console.log('submit');
+  }
+  const onCancel = () => {
+    //TODO: implement form cancel
+    console.log('cancel');
+  }
+  //TODO: style form
+  return (
+    <div>
+      <h1>New HealthCheck entry</h1>
+      <form>
+        <div>
+          <label>description</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>date</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>specialist</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>healthCheckRating</label>
+          <input type="text" />
+        </div>
+        <button type="submit">Add</button>
+        <button type="button">Cancel</button> 
+      </form>
+    </div>
+  )
+};
+
+export default EntryForm;
