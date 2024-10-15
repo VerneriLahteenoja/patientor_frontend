2602eed2d22de3f627d962c7579e2b229787f6f0
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-10-15 17:42:41 +0300

    gix set onChanges

--- src/components/PatientPage/entryForm.tsx
+++ src/components/PatientPage/entryForm.tsx
@@ -11,13 +11,13 @@ const EntryForm = () => {
     //TODO: implement form submit
     event.preventDefault();
     console.log('submit');
-  }
+  };
   const onCancel = () => {
     setDescription('');
     setDate('');
     setSpecialist('');
     setHealthCheckRating('');
-  }
+  };
   //TODO: style form
   return (
     <div>
@@ -29,21 +29,21 @@ const EntryForm = () => {
         </div>
         <div>
           <label>date</label>
-          <input type="text" value={date} onChange={({target}) => setDescription(target.value)} />
+          <input type="text" value={date} onChange={({target}) => setDate(target.value)} />
         </div>
         <div>
           <label>specialist</label>
-          <input type="text" value={specialist} onChange={({target}) => setDescription(target.value)} />
+          <input type="text" value={specialist} onChange={({target}) => setSpecialist(target.value)} />
         </div>
         <div>
           <label>healthCheckRating</label>
-          <input type="text" value={healthCheckRating} onChange={({target}) => setDescription(target.value)} />
+          <input type="text" value={healthCheckRating} onChange={({target}) => setHealthCheckRating(target.value)} />
         </div>
         <button type="submit">Add</button>
         <button type="button" onClick={() => oncancel}>Cancel</button> 
       </form>
     </div>
-  )
+  );
 };
 
 export default EntryForm;
